8fef8e1bf114e57f8f3948e0b0420317e8ae5178
Joseph Palmer <75081147+BlockBuilderJoe@users.noreply.github.com>
2024-10-02 11:35:27 +0100

    changed versioning of the resource pack and added the uuid adder to the translator

--- translation/makePack.sh
+++ translation/makePack.sh
@@ -12,12 +12,22 @@ if [ ! -d "Cadw" ]; then
     exit 1
 fi
 
+# Generate new UUIDs
+header_uuid=$(uuidgen)
+module_uuid=$(uuidgen)
+
+# Update manifest.json with new UUIDs
+sed -i "s/\"uuid\": \"[0-9a-f-]*\",/\"uuid\": \"$header_uuid\",/" Cadw/manifest.json
+sed -i "s/\"uuid\": \"[0-9a-f-]*\",/\"uuid\": \"$module_uuid\",/" Cadw/manifest.json
+
 # Create the mcpack file
 zip -r Cadw.mcpack Cadw
 
 # Check if the zip command was successful
 if [ $? -eq 0 ]; then
-    echo "Successfully created Cadw.mcpack"
+    echo "Successfully created Cadw.mcpack with updated UUIDs"
+    echo "Header UUID: $header_uuid"
+    echo "Module UUID: $module_uuid"
 else
     echo "Error: Failed to create Cadw.mcpack"
     exit 1
